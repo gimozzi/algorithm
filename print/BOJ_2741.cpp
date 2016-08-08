c23730af2582e9421df6d1898219e96d06d46324
gimozzi <javarujavara@gmail.com>
2016-08-08 07:22:14 +0000

    print: endl 대신 \n 사용

--- print/BOJ_2741.cpp
+++ print/BOJ_2741.cpp
@@ -8,6 +8,6 @@ int main (void) {
     int N;
     cin >> N;
     for(int i=0 ; i<N ; i++)
-        cout << i+1 << endl;
+        cout << i+1 << "\n";    //endl 은 버퍼 flush 때문에 반복하면 느림
     return 0;
 }
--- print/BOJ_2742.cpp
+++ print/BOJ_2742.cpp
@@ -9,7 +9,7 @@ int main (void) {
     cin >> N;
 
     for(int i=N ; i>0 ; i--)
-        cout << i << endl;
+        cout << i << "\n";  //endl 대신 \n 사용
 
     return 0;
 }
