7420f776314d1db77242c207d2973d6573fff4a1
gimozzi <javarujavara@gmail.com>
2016-08-10 06:44:12 +0000

    ****아직 푸는 중이욤...

---
+++ workbook_704/BOJ_3040.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main (void) {
+
+    int v[9];
+    int mat[9 * 8][7 + 1] = {0, };
+
+    for(int i=0 ; i<9 ; i++)
+        cin >> v[i];
+
+    /*
+    for(int i=0 ; i<7 ; i++){
+        for(int j=0 ; j<9 ; j++){
+            for(int k=0 ; k ; k++){
+                mat[][i] = v[j];
+            }            
+        }
+    }
+*/
+    for(int i=0 ; i<9*8 ; i++){
+
+        mat[i][] = v[];
+
+    }
+    
+
+    
+
+    return 0;
+}
