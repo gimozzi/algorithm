c5032e00aab53dbd487ebcacdc455cfded3588e0
gimozzi <javarujavara@gmail.com>
2016-08-12 05:19:13 +0000

    1.reverse strnig  2.string to int  3.ternary operator

--- easy/BOJ_2908.cpp
+++ easy/BOJ_2908.cpp
@@ -18,7 +18,7 @@ int main (void) {
     //int a = stoi(str1);   // c++11
     //int b = stoi(str2);   // c++11
     int a = atoi(str1.c_str());
-    int b = atoi(str1.c_str());
+    int b = atoi(str2.c_str());
 
     // ternary operator
     //cout << a > b ? a : b;
