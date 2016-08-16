552c8aa29527c9f8a73b931c128e4d9f445962c1
gimozzi <javarujavara@gmail.com>
2016-08-16 06:56:40 +0000

    수 범위 맞추기 어렵다...ㅠㅠ (unsigned long long 으로 통일하였음)

--- easy/BOJ_10539.cpp
+++ easy/BOJ_10539.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cstdio>
 
 using namespace std;
 
@@ -9,14 +10,16 @@ int main (void) {
     int N;
     cin >> N;
 
-    vector<int> v(N);
+    //vector<int> v(N);     //이렇게 하면 안되요...
+    vector<unsigned long long> v(N);
     for(int i=0 ; i<N ; i++)
         cin >> v[i];
     
-    long long l = 0;
+    unsigned long long l = 0;
     for(int i=0 ; i<N ; i++){
-        cout << v[i] * (i+1) - l << " ";
-        l += v[i] * (i+1) - l;
+//        cout << (unsigned long long)(v[i] * (i+1) - l) << " ";
+        printf("%llu ", (unsigned long long)(v[i] * (i+1) -l));
+        l += (unsigned long long)(v[i] * (i+1) - l);
     }
 
 
