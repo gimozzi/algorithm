b72b454b92477d2d083128dcd8e486238d9cd7fd
gimozzi <javarujavara@gmail.com>
2016-08-16 08:50:00 +0000

    돌

---
+++ easy/BOJ_9655.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+
+int main (void) {
+    int N;
+    scanf("%d", &N);
+    if(N%2 == 1)
+        printf("SK");
+    else
+        printf("CY");
+    return 0;
+}
---
+++ easy/BOJ_9656.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+
+int main (void) {
+    int N;
+    scanf("%d", &N);
+    if(N%2 == 1)
+        printf("CY");
+    else
+        printf("SK");
+    return 0;
+}
