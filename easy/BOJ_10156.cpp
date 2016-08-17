f3f93c0116c6d8e7ea55f9cbe42fefab7e0d2d63
gimozzi <javarujavara@gmail.com>
2016-08-17 05:09:26 +0000

    Workbook: 566

---
+++ easy/BOJ_10156.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main (void) {
+
+    int K, N, M;
+    cin >> K >> N >> M;
+
+    if( (K*N) < M)
+        cout << 0 << endl;
+    else
+        cout << K * N - M << endl;
+
+    return 0;
+}
