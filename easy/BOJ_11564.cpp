08606245f0abd234e4ec9038a7ce72a36fa7c208
gimozzi <javarujavara@gmail.com>
2016-08-17 03:37:15 +0000

    닫힌 구간에서 배수 구하기, long long

--- easy/BOJ_11564.cpp
+++ easy/BOJ_11564.cpp
@@ -3,12 +3,18 @@
 
 int main (void) {
 
-    long long k, a, b, i, cnt = 0;
+    long long k, a, b, cnt = 0;
     scanf("%lld %lld %lld", &k, &a, &b);
-
+/*
+    // 5 4 11 인 경우 안됨
     cnt = (b-a)/k;
     if( (b%k == 0) || (a%k == 0) )
         cnt++;
+*/
+    cnt = b/k - a/k;
+    if( (b%k == 0) || (a%k == 0) || (a<0 && 0<b) )
+        cnt++;
+    
     printf("%lld\n", cnt);
     
     return 0;
