5f53e7c60e313b689b59bcf62db67483ddf94972
gimozzi <javarujavara@gmail.com>
2016-08-17 04:56:46 +0000

    STL sort

---
+++ easy/BOJ_2480.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main (void) {
+    
+    vector<int> v(3);
+    for(int i=0 ; i<3 ; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end(), greater<int>());
+    int repeat = 1, money = 0;
+    if(v[0] == v[1]) repeat++;
+    if(v[1] == v[2]) repeat++;
+
+    switch(repeat){
+    case 1:
+        money = v[0] * 100;
+        break;
+    case 2:
+        money = 1000 + v[1] * 100;
+        break;        
+    case 3:
+        money = 10000 + v[1] * 1000;
+        break;
+    }
+    cout << money << endl;
+
+    return 0;
+}
