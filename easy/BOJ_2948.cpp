588b7c43b61dfe528928b07b67b81e8e3ff6adb8
gimozzi <javarujavara@gmail.com>
2016-08-18 03:31:07 +0000

    calendar

---
+++ easy/BOJ_2948.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main (void) {
+
+    string day[7] = {"Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
+    int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int M, D, date = 0;
+    cin >> M >> D;
+    for(int i=1 ; i<M ; i++)
+        date += month[i-1];
+    date += D;
+
+    cout << day[date % 7] << endl;
+
+    return 0;
+}
