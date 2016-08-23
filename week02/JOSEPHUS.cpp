fcbd1f81c18cc19e2f354987aac18be2f5fc8b60
gimozzi <javarujavara@gmail.com>
2016-08-23 14:29:59 +0000

    linked list

---
+++ week02/JOSEPHUS.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+
+void josephus (int n, int k) {
+    
+    list<int> survivors;
+
+    for(int i=1 ; i<=n ; ++i)
+        survivors.push_back(i);
+
+    list<int>::iterator kill = survivors.begin();
+    while(n > 2){
+        kill = survivors.erase(kill);
+        
+        if(kill == survivors.end())
+            kill = survivors.begin();
+        --n;
+
+        for(int i=0 ; i<k-1 ; ++i){
+            ++kill;
+            if(kill == survivors.end())
+                kill = survivors.begin();
+        }
+    }
+
+    cout << survivors.front() << " " << survivors.back() << endl;
+        
+}
+
+
+int main (void) {
+    
+    int C;
+    cin >> C;
+    while(C--){
+        int N, K;
+        cin >> N >> K;
+
+        josephus(N, K);    
+    }
+
+    return 0;
+}
